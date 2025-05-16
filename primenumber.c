78b4e1492e84bf6ed3c8bb09627d4637561fb29b
Bharathv2005 <bharath.vreddy06@gmail.com>
2025-05-16 11:00:31 +0530

    Create primenumber.c
    Signed-off-by: Bharathv2005 <bharath.vreddy06@gmail.com>

---
+++ primenumber.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int i,num,isprime=1;
+    printf("enter the value of num:");
+    scanf("%d",&num);
+    for(i=2;i=num/2;i++){
+        if (num%2==0){
+            printf("the number is not prime");
+        }
+        else{
+        printf("the number is prime");
+    }
+    }
+    
+}
